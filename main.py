026c32c4f8616f01e3eafdfd6f6cd447e0e8d45f
wujinhao <148603373+ATRIOR-LCL@users.noreply.github.com>
2026-03-12 20:58:41 +0800

    upd: add host and port to start environment

--- main.py
+++ main.py
@@ -398,4 +398,6 @@ def serve_spa() -> Any:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    host = os.environ.get("HOST", "0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host=host, port=port)
